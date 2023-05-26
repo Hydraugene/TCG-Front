da9f7889560d93c5255d901401ec3d844a6d8b1a
Thomas Ravenne <ravennetho@cy-tech.fr>
2023-05-26 10:25:43 +0200

    initialisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TCG-front';
+  title = 'Défaut';
+
+  public token: string;
+
+  constructor(private http: HttpClient) {
+    this.token = "";
+   }
+
+  public testfn(){
+    this.login()
+  }
+
+  public async login(){
+    var token : string;
+    token = "";
+
+    // //await this.http.get(process.env['API_HOST'] + '/').subscribe((res) => {
+    // await this.http.get('http://localhost:3000/').subscribe((res) => {
+    //     //token = JSON.stringify(Object.values(res)[0]);
+    //   //console.log(token);
+    //   console.log(res)
+    // });
+
+    await this.http.get('http://localhost:3000', { responseType: 'text' }).subscribe(res => {
+      console.log(res)
+      this.title = res
+    });
+
+    return token;
+  }
+
 }
